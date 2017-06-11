61de2057cc4232f81d72bdfcbd89f673959f6955
Jarred Parr <jparr721@gmail.com>
2017-06-11 17:30:34 -0400

    got the dropdown working! Onto the Django...

--- Login/login.js
+++ Login/login.js
@@ -2,3 +2,20 @@ $(document).ready(function () {
   $('div.login-box').fadeIn(2000).removeClass('.login-box');
 
 });
+
+var username = ["G01348213"];
+var password = ["1234"];
+
+
+
+function checkUsernameAndPassword(username, password) {
+  this.username = document.getElementByID('inputEmail');
+  this.password = document.getElementByID('inputPassword');
+  if (this.username != username || this.password != password){
+    console.log("error!");
+    alert("incorrect username and password")
+  } else {
+    console.log("Success");
+    location.href="../Dashboard/index.html"
+  }
+}
